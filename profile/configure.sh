917934ea1aeb26b468fa256b1320da0d69aed263
Jim Casey <jim.casey@gmail.com>
2020-04-28 18:45:15 -0600

    Added profile and zsh configuration (#2)

---
+++ profile/configure.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo_action() {
+  echo
+  echo -e "${bright}${1}${normal}"
+}
+
+bright="\033[1;36m"
+normal="\033[0m"
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+zshrc_local=~/.zshrc
+zshrc_command="source ${script_dir}/.zshrc"
+
+if ! grep -qs "$zshrc_command" $zshrc_local; then
+  echo_action "Configuring ${zshrc_local}..."
+  touch $zshrc_local
+  echo "export PROFILE_FOLDER=${script_dir}" >> $zshrc_local
+  echo $zshrc_command >> $zshrc_local
+fi
+
+echo_action "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+zshrc_backup=~/.zshrc.pre-oh-my-zsh
+if [[ -f $zshrc_backup ]]; then
+  echo_action "Restoring ${zshrc_local}..."
+  rm $zshrc_local
+  mv $zshrc_backup $zshrc_local
+fi
+
+echo "Done."
