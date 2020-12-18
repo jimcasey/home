669e1c9bca026b6de287a5ddd9e54b4164e184fb
Jim Casey <jim.casey@gmail.com>
2020-12-18 15:51:14 -0700

    Day 18

--- projects/advent-of-code/2020/18/index.js
+++ projects/advent-of-code/2020/18/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const TESTING = false // 26457
+const TESTING = false // 694173
 const INPUT_PATH = `${__dirname}/${TESTING ? 'test' : 'input'}.txt`
 
 const split = (str, separator = '\n') => str.split(separator).filter((s) => !!s)
@@ -10,78 +10,66 @@ const readInput = async () => {
   return split(file)
 }
 
-const OPER_ADD = '+'
-const OPER_MULTIPLY = '*'
-
 const parseExpr = (expr) => {
   const ops = []
+  let parenCount = 0
+  let parenStart
   for (let index = 0; index < expr.length; index++) {
     const char = expr[index]
-    const oper = expr[index - 1]
 
-    let value
-    if (Number(char) !== NaN) value = Number(char)
     if (char === '(') {
-      const { subOps, offset } = parseSubExpr(expr, index)
-      index += offset
-      value = subOps
+      if (parenCount === 0) parenStart = index + 1
+      parenCount++
     }
 
-    index++
-    ops.push({
-      value,
-      oper,
-    })
+    if (parenCount === 0) {
+      const num = Number(char)
+      ops.push(isNaN(num) ? char : num)
+    }
+
+    if (char === ')') {
+      parenCount--
+      if (parenCount === 0) {
+        ops.push(parseExpr(expr.substring(parenStart, index)))
+        parenStart = undefined
+      }
+    }
   }
   return ops
 }
 
-const parseSubExpr = (expr, index) => {
-  let parenCount = 0
-  const subExpr = expr
-    .substr(index + 1)
-    .split('')
-    .reduce((e, c) => {
-      if (parenCount < 0) return e
-      switch (c) {
-        case '(':
-          parenCount++
-          break
-        case ')':
-          parenCount--
-          break
-      }
-      if (parenCount < 0) return e
-      return e + c
-    }, '')
+const evaluateExpr = (ops) => {
+  const evalOps = []
 
-  return {
-    subOps: parseExpr(subExpr),
-    offset: subExpr.length + 1,
-  }
-}
-
-const evaluateExpr = (expr) => {
-  const ops = Array.isArray(expr) ? expr : parseExpr(expr)
-  return ops.reduce((n, { oper, value }) => {
-    const v = Array.isArray(value) ? evaluateExpr(value) : value
-    switch (oper) {
-      case OPER_MULTIPLY:
-        return n * v
-      case OPER_ADD:
-      default:
-        return n + v
+  for (let index = 0; index < ops.length; index++) {
+    const op = ops[index]
+    if (op === '+') {
+      index++
+      evalOps[evalOps.length - 1] += evaluateOp(ops[index])
+    } else {
+      evalOps.push(evaluateOp(op))
     }
-  }, 0)
+  }
+
+  let answer = evalOps[0]
+  for (let index = 1; index < evalOps.length; index += 2) {
+    answer *= evalOps[index + 1]
+  }
+
+  return answer
 }
+
+const evaluateOp = (op) => (Array.isArray(op) ? evaluateExpr(op) : op)
 
 const run = async () => {
   const input = await readInput()
   const exprs = input.map((expr) => expr.replaceAll(' ', ''))
 
-  const answer = exprs.reduce((sum, expr) => sum + evaluateExpr(expr), 0)
-
-  console.log(`The sum of all expressions is ${answer}`)
+  const answer = exprs.reduce(
+    (n, expr) => (n += evaluateExpr(parseExpr(expr))),
+    0,
+  )
+  console.log(`The sum of all evaluated expressions is ${answer}.`)
 }
 
 run()
