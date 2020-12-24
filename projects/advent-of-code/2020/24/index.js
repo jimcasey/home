f2603875619e1198e92c2fe49f23cb295194444a
Jim Casey <jim.casey@gmail.com>
2020-12-24 21:22:04 +0000

    Day 24 part 2

--- projects/advent-of-code/2020/24/index.js
+++ projects/advent-of-code/2020/24/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const util = require('util')
 
-const TESTING = !!process.env.TEST // 20
+const ITERATIONS = 16
+
+const TESTING = !!process.env.TEST // 2208
 const INPUT_PATH = `${__dirname}/${TESTING ? 'test' : 'input'}.txt`
 
 const split = (str, separator = '\n') => str.split(separator).filter((s) => !!s)
@@ -16,14 +18,19 @@ const run = async () => {
   const input = await readInput()
   const instructions = input.map(parseDirections)
   const positions = instructions.map(getPosition)
+  let tiles = positions.reduce(flipTile, {})
 
-  const tiles = positions
-    .map((position) => position.join(','))
-    .reduce((map, id) => ({ ...map, [id]: !map[id] }), {})
+  for (let n = 0; n < ITERATIONS; n++) {
+    tiles = getEmptyTiles(tiles).reduce(addEmptyTile, tiles)
+    tiles = Object.keys(tiles)
+      .map(parseTileID)
+      .filter((position) => shouldFlip(tiles, position))
+      .reduce(flipTile, tiles)
+  }
 
-  const count = Object.values(tiles).filter((state) => state).length
+  const count = Object.values(tiles).filter((state) => !!state).length
 
-  console.log(`Floor contains ${count} black tiles.`)
+  console.log(`There are ${count} black tiles after ${ITERATIONS} days.`)
 }
 
 const parseDirections = (input) =>
@@ -33,25 +40,70 @@ const parseDirections = (input) =>
   )
 
 const getPosition = (directions) =>
-  directions
-    .map(getOffset)
-    .reduce(([x, y, z], [x1, y1, z1]) => [x + x1, y + y1, z + z1], [0, 0, 0])
+  directions.map(getOffset).reduce(applyOffset, [0, 0, 0])
 
-const getOffset = (direction) => {
-  switch (direction) {
-    case 'ne':
-      return [0, 1, 1]
-    case 'e':
-      return [1, 1, 0]
-    case 'se':
-      return [1, 0, -1]
-    case 'sw':
-      return [0, -1, -1]
-    case 'w':
-      return [-1, -1, 0]
-    case 'nw':
-      return [-1, 0, 1]
+const OFFSETS = {
+  ne: [0, 1, 1],
+  e: [1, 1, 0],
+  se: [1, 0, -1],
+  sw: [0, -1, -1],
+  w: [-1, -1, 0],
+  nw: [-1, 0, 1],
+}
+
+const getOffset = (direction) => OFFSETS[direction]
+const getOffsetPositions = (position) =>
+  Object.values(OFFSETS).map((offset) => applyOffset(position, offset))
+const applyOffset = ([x, y, z], [x1, y1, z1]) => [x + x1, y + y1, z + z1]
+
+const getTileID = (position) => position.join(',')
+const parseTileID = (id) => id.split(',').map(Number)
+const getTileState = (tiles, position) => !!tiles[getTileID(position)]
+const addEmptyTile = (tiles, position) => ({
+  ...tiles,
+  [getTileID(position)]: false,
+})
+const flipTile = (tiles, position) => ({
+  ...tiles,
+  [getTileID(position)]: !getTileState(tiles, position),
+})
+
+const getAdjacentCount = (tiles, position) =>
+  getOffsetPositions(position).reduce(
+    (count, positionO) => count + (getTileState(tiles, positionO) ? 1 : 0),
+    0,
+  )
+
+const shouldFlip = (tiles, position) => {
+  const count = getAdjacentCount(tiles, position)
+  switch (getTileState(tiles, position)) {
+    case true:
+      return count === 0 || count > 2
+    case false:
+      return count === 2
   }
 }
 
+const getEmptyTiles = (tiles) =>
+  Object.keys(tiles)
+    .map(parseTileID)
+    .reduce(
+      (positions, position) => [
+        ...positions,
+        ...getOffsetPositions(position).filter(
+          (positionO) => !getTileState(tiles, positionO),
+        ),
+      ],
+      [],
+    )
+
 run()
+
+const EXEC_TIME = {}
+const execTime = (key, fn) => {
+  const startMs = Date.now()
+  const result = fn()
+  if (!EXEC_TIME[key]) EXEC_TIME[key] = 0
+  EXEC_TIME[key] += Date.now() - startMs
+  return result
+}
