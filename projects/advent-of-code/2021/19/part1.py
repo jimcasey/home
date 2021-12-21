7ed022dc402cdf58865b281103b89dd4fa004890
Jim Casey <jim.casey@gmail.com>
2021-12-21 13:52:38 -0700

    Advent of Code 2021 19.1

--- projects/advent-of-code/2021/19/part1.py
+++ projects/advent-of-code/2021/19/part1.py
@@ -1,8 +1,36 @@
+from collections import defaultdict
+from math import sqrt
 import os
 
 scriptPath = os.path.dirname(os.path.abspath(__file__))
-inputPath = scriptPath + '/test.txt'
+inputPath = scriptPath + '/input.txt'
 
 with open(inputPath) as file:
-  input = file.read()
-  print(input)
+  scanners = [
+      [tuple(map(int, line.split(','))) for line in group.split('\n')[1:]]
+      for group in file.read().strip().split('\n\n')
+  ]
+
+def distance(beaconA, beaconB):
+  xA, yA, zA = beaconA
+  xB, yB, zB = beaconB
+  return sqrt(pow(xA - xB, 2) + pow(yA - yB, 2) + pow(zA - zB, 2) * 1.0)
+
+unique = set()
+
+for beacons in scanners:
+  for beacon in beacons:
+    neighbors = {}
+    for peer in beacons:
+      if peer != beacon:
+        neighbors[distance(beacon, peer)] = peer
+    distanceA, distanceB = sorted(neighbors)[0:2]
+    neighborA = neighbors[distanceA]
+    neighborB = neighbors[distanceB]
+    key = (distanceA + distanceB) * distance(neighborA, neighborB)
+    unique.add(key)
+
+print(len(unique))
+
+# credit where credit is due on this one:
+# https://bit.ly/3yOF591
