e243847f755647d8a45e54b16a08329660e79e1f
Jim Casey <jim.casey@gmail.com>
2022-02-03 15:15:11 -0700

    Added codea to aliases

--- profile/aliases.sh
+++ profile/aliases.sh
@@ -40,4 +40,5 @@ alias dps="docker ps --format \"table {{.ID}}\t{{.Names}}\t{{.Command}}\t{{.Stat
 alias denode='$(TMP=$(mktemp -d) && mv -f node_modules $TMP && rm -rf $TMP) &> /dev/null &'
 
 # VSCode
+alias codea='code --add'
 alias codew='code --reuse-window'
