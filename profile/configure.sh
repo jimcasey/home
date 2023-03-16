abd09c60afee13ab5cd551fcc3e5e25e203ebbe0
Jim Casey <jim.casey@gmail.com>
2023-03-16 11:04:04 -0600

    Update configure.sh to include vim and bat theming

--- profile/configure.sh
+++ profile/configure.sh
@@ -5,11 +5,18 @@ echo_action() {
   echo -e "${bright}${1}${normal}"
 }
 
+append() {
+  if ! grep -qs "\"$1\"" "$2"; then
+    echo "$1" >> "$2"
+  fi
+}
+
 bright="\033[1;36m"
 normal="\033[0m"
 
 script_dir="$(cd "$(dirname ${BASH_SOURCE[0]-$0})" && pwd)"
 
+echo_action "Configuring Zsh..."
 ohmyzsh_path=~/.oh-my-zsh
 if [[ ! -d $ohmyzsh_path ]]; then
   echo_action "Installing Oh My Zsh..."
@@ -22,7 +29,7 @@ zshrc_command="source ${script_dir}/.zshrc"
 if ! grep -qs "$zshrc_command" $zshrc_local; then
   echo_action "Configuring ${zshrc_local}..."
   touch $zshrc_local
-  echo $zshrc_command >> $zshrc_local
+  append $zshrc_local $zshrc_command
 fi
 
 zshrc_backup=~/.zshrc.pre-oh-my-zsh
@@ -38,5 +45,24 @@ git config --global core.pager "cat"
 git config --global pager.branch "false"
 git config --global pager.diff "false"
 git config --global log.diff "false"
+
+echo_action "Configuring vim..."
+if [[ ! -d ~/.vim/pack/themes/start/vim-code-dark ]]; then
+  mkdir -p ~/.vim/pack/themes/start
+  cd ~/.vim/pack/themes/start
+  git clone https://github.com/tomasiser/vim-code-dark
+fi
+
+touch ~/.vimrc
+append "syntax enable" ~/.vimrc
+append "colorscheme codedark" ~/.vimrc
+
+echo_action "Configuring bat..."
+if [[ ! -d ~/.config/bat ]]; then
+  mkdir -p ~/.config/bat
+fi
+
+touch ~/.config/bat/config
+append '--theme="Visual Studio Dark+"' ~/.config/bat/config
 
 echo "Done."
