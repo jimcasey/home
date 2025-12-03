f93a6848708eb1758a8b2f67ddb997faa03b44ee
Jim Casey <jim.casey@gmail.com>
2025-12-03 06:38:49 -0700

    AoC 2025 Day 02 (#23)

---
+++ projects/advent-of-code/2025/02/part1.py
@@ -0,0 +1,33 @@
+from os import path
+
+scriptPath = path.dirname(path.abspath(__file__))
+inputPath = scriptPath + '/input.txt'
+
+
+def main():
+  line = readInput()
+  items = line.split(',')
+  total = 0
+  for item in items:
+    start, end = item.split('-')
+    for num in range(int(start), int(end) + 1):
+      if is_mirrored(num):
+        total += num
+  print(total)
+
+
+def is_mirrored(num):
+  s = str(num)
+  length = len(s)
+  if length % 2 != 0:
+    return False
+  mid = length // 2
+  return s[:mid] == s[mid:]
+
+
+def readInput():
+  with open(inputPath) as file:
+    return file.read().strip()
+
+
+main()
